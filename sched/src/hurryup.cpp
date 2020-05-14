46ca654977ee5cdeb337d19e6f5c44e1cb81a757
raijenki <daniel@kuriate.com.br>
2020-05-14 01:38:34 +0000

    code changes

--- sched/src/hurryup.cpp
+++ sched/src/hurryup.cpp
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string>
+#include <sys/time.h>
 
 static std::thread scheduling_thread;
 int fd[24];
@@ -50,7 +51,7 @@ void hurryup_init()
         while(!should_stop_scheduler.load(std::memory_order_relaxed))
         {
             using namespace std::chrono_literals;
-            std::this_thread::sleep_for(10ms);
+            std::this_thread::sleep_for(20ms);
             hurryup_tick();
         }
 
@@ -71,14 +72,24 @@ void hurryup_shutdown()
 }
 
 void hurryup_freqchange(void) {
+	struct timeval  tv;
 	
 	for(auto i = 0; i < 23; i=i+2) { 
 		if(changes[i] == 0) {
 			char freq[8] = "1000000";
+			gettimeofday(&tv, NULL);
+			double time_in_mill = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ; 
+			//std::cout << "change to 1.0 at " << time_in_mill << std::endl;
 			write(fd[i], freq, strlen(freq));
+			changes[i] = 2; // Don't change again until necessary
 		} else if (changes[i] == 1) {
 			char freq[8] = "2600000";
+			gettimeofday(&tv, NULL);
+			double time_in_mill = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ; 
+			printf("%lf\n", time_in_mill);
+			//std::cout << "change to 2.6 at " << time_in_mill << std::endl;
 			write(fd[i], freq, strlen(freq));
+			changes[i] = 2; // Don't change again
 			}
 		}
 }
@@ -95,10 +106,9 @@ void hurryup_tick() {
 	// It exists
 	if (it != es_threads.end()) {
 		// On the same processor, do the processing
-			// Check if hotpath is zero
+			// It is NOT on the hopath, thus we should change the frequency to minimum
 			if(ct_item.is_hotpath == 0) {
 				std::get<1>(*it) = ct_item.cpu_id;
-			// Don't even bother, change freq to 1.0GHz and update tuple
 				changes[ct_item.cpu_id] = 0;
 				//std::cout << "Freq change to 1.0ghz: core " << std::get<1>(*it) << std::endl;
 				std::get<2>(*it) = ct_item.timestamp;
@@ -107,7 +117,7 @@ void hurryup_tick() {
 			}
 			// It IS on hotpath
 			else {	
-				// First time?
+				// First time on hotpath?
 				if(std::get<4>(*it) == 0) {
 					std::get<3>(*it) = 0;
 					std::get<4>(*it) = 1;
@@ -118,8 +128,9 @@ void hurryup_tick() {
 				std::get<1>(*it) = ct_item.cpu_id;
 				std::get<3>(*it) += (ct_item.timestamp - std::get<2>(*it));
 				std::get<2>(*it) = ct_item.timestamp;
-				if(std::get<3>(*it) > 350000000) {
-					std::cout << "freq change!" << std::endl;
+				if((std::get<3>(*it) > 300000000) && (std::get<4>(*it) == 1)) { // For debbuging purposes 
+					//std::cout << "freq change!" << std::endl;
+					std::get<4>(*it) = 2; // ACTIVE ON HOT FUNCTION, WITH FREQ CHANge
 					changes[ct_item.cpu_id] = 1;
 				}
 
